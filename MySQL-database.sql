97340a013da4db477e233febadcab6cac0594ad1
Phong <phongkaster@gmail.com>
2022-07-04 07:25:00 +0700

    04-07-2022 cập nhật database script mới

--- MySQL-database.sql
+++ MySQL-database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 02, 2022 at 10:19 AM
+-- Generation Time: Jul 04, 2022 at 02:22 AM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 7.4.29
 
@@ -41,9 +41,29 @@ CREATE TABLE `ec_categories` (
 --
 
 INSERT INTO `ec_categories` (`id`, `name`, `description`, `position`, `parent_id`, `slug`) VALUES
-(1, 'Dell', 'Máy tính Dell đến từ nước Mỹ', 0, 0, 'may-tinh-dell'),
-(2, 'ASUS', 'Máy tính ASUS đền từ Đài Loan', 1, 0, 'may-tinh-asus'),
-(3, 'Lenovo', 'Máy tính lenovo đến từ Trung Quốc', 2, 0, 'may-tinh-lenovo');
+(1, 'Unknown', 'The category for products that aren\'t classified !', 0, 0, 'unknown'),
+(2, 'ASUS', 'ASUS from Taiwan', 1, 0, 'asus'),
+(3, 'Lenovo', 'Lenovo from China', 2, 0, 'lenovo');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ec_configuration`
+--
+
+CREATE TABLE `ec_configuration` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `data` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `ec_configuration`
+--
+
+INSERT INTO `ec_configuration` (`id`, `name`, `data`) VALUES
+(1, 'smtp', '{\r\n  \"host\": \"smtp.gmail.com\",\r\n  \"port\": \"465\",\r\n  \"encryption\": \"ssl\",\r\n  \"auth\": true,\r\n  \"username\": \"phongkaster@gmail.com\",\r\n  \"password\": \"def502007f265790521f109cc8d371201ce1d3ea6b50a36aa964cc12ca24d71e7664e379ab7a24b5efa3dd21b1227915b8db9e02c54be13af67ac67c7a38002713c6088f6db45b3070e5dee82504ae1ada01343f156044d87301678335f3eaf4eaaf\",\r\n  \"from\": \"phongkaster@gmail.com\"\r\n}'),
+(2, 'settings', '{\r\n  \"site_name\": \"PhongKaster Shop\",\r\n  \"site_description\": \"A shop provides PC/Laptop\",\r\n  \"site_keywords\": \"pc, laptop, personal laptop\",\r\n  \"currency\": \"đ\",\r\n  \"logomark\": \"https://timeswriter.xyz/assets/uploads/images/logo.png\",\r\n  \"logotype\": \"https://timeswriter.xyz/assets/uploads/images/logo.png\",\r\n  \"site_slogan\": \"Together for the future\",\r\n  \"language\": \"vi-VN\"\r\n}');
 
 -- --------------------------------------------------------
 
@@ -89,7 +109,7 @@ CREATE TABLE `ec_products` (
   `remaining` int(11) DEFAULT 0,
   `manufacturer` varchar(30) DEFAULT NULL,
   `price` int(11) DEFAULT 0,
-  `screen_size` varchar(10) DEFAULT NULL,
+  `screen_size` float DEFAULT NULL,
   `cpu` varchar(50) DEFAULT NULL,
   `ram` varchar(10) DEFAULT NULL,
   `graphic_card` varchar(50) DEFAULT NULL,
@@ -105,9 +125,11 @@ CREATE TABLE `ec_products` (
 --
 
 INSERT INTO `ec_products` (`id`, `name`, `remaining`, `manufacturer`, `price`, `screen_size`, `cpu`, `ram`, `graphic_card`, `rom`, `demand`, `content`, `create_at`, `update_at`) VALUES
-(1, 'Asus TUF Gaming FX506LHB-HN188W i5 10300H', 15, 'ASUS', 24990000, '15.6', 'Intel, Core i5, 10300H', '8GB', 'NVIDIA GeForce GTX 1', '512GB', 'Gaming', 'ASUS TUF Gaming FX506LHB HN188W là chiếc laptop gaming giá rẻ với thiết kế tuyệt đẹp, phong cách chuẩn game thủ và cấu hình mạnh mẽ cho cả học tập, công việc cũng như chơi game. Bên cạnh đó là độ bền chuẩn quân đội đã làm nên tên tuổi của dòng TUF.', NULL, NULL),
-(2, 'Laptop Lenovo IdeaPad Slim 5 15ITL05 i5 1135G7/8GB', 10, 'Lenovo', 14999000, '15.6', 'Intel Core i5 1', '8GB', 'Intel Iris Xe', '512GB', 'Văn phòng', 'Dòng Lenovo IdeaPad Slim 5 15ITL05 giờ đây đã được nâng cấp lên bộ vi xử lý Intel thế hệ thứ 11 vô cùng mạnh mẽ, trở thành chiếc laptop 15,6 inch nhỏ gọn, hiệu suất chuyên nghiệp, lý tưởng cho công việc.', '2022-06-30 22:13:16', '2022-06-30 22:13:16'),
-(3, 'Laptop Dell Vostro 3500', 10, 'Lenovo', 17999000, '15.6', 'Intel Core i7 1165G7', '8GB', 'NVIDIA GeForce MX330', '512GB', 'Van-phong', 'Dòng Dell vostro 3500 giờ đây đã được nâng cấp lên bộ vi xử lý Intel thế hệ thứ 11 vô cùng mạnh mẽ, trở thành chiếc laptop 15,6 inch nhỏ gọn, hiệu suất chuyên nghiệp, lý tưởng cho công việc.', '2022-06-30 22:13:34', '2022-06-30 22:13:34');
+(1, 'Asus TUF Gaming FX506LHB-HN188W i5 10300H', 15, 'ASUS', 24990000, 15.6, 'Intel, Core i5, 10300H', '8GB', 'NVIDIA GeForce GTX 1', '512GB', 'Gaming', 'ASUS TUF Gaming FX506LHB HN188W là chiếc laptop gaming giá rẻ với thiết kế tuyệt đẹp, phong cách chuẩn game thủ và cấu hình mạnh mẽ cho cả học tập, công việc cũng như chơi game. Bên cạnh đó là độ bền chuẩn quân đội đã làm nên tên tuổi của dòng TUF.', NULL, NULL),
+(2, 'Laptop Lenovo IdeaPad Slim 5 15ITL05 i5 1135G7/8GB', 10, 'Lenovo', 14999000, 15.6, 'Intel Core i5 1', '8GB', 'Intel Iris Xe', '512GB', 'Văn phòng', 'Dòng Lenovo IdeaPad Slim 5 15ITL05 giờ đây đã được nâng cấp lên bộ vi xử lý Intel thế hệ thứ 11 vô cùng mạnh mẽ, trở thành chiếc laptop 15,6 inch nhỏ gọn, hiệu suất chuyên nghiệp, lý tưởng cho công việc.', '2022-06-30 22:13:16', '2022-06-30 22:13:16'),
+(3, 'Laptop Dell Vostro 3500', 10, 'Lenovo', 17999000, 15.6, 'Intel Core i7 1165G7', '8GB', 'NVIDIA GeForce MX330', '512GB', 'Van-phong', 'Dòng Dell vostro 3500 giờ đây đã được nâng cấp lên bộ vi xử lý Intel thế hệ thứ 11 vô cùng mạnh mẽ, trở thành chiếc laptop 15,6 inch nhỏ gọn, hiệu suất chuyên nghiệp, lý tưởng cho công việc.', '2022-06-30 22:13:34', '2022-06-30 22:13:34'),
+(4, 'Laptop Dell Vostroll 34', 0, 'Dell Techonology', 2000000, 14, 'Intel Core I7-1165G7', '8', 'Intel Irisxs, MX 330', '512GB', 'office', '0', '2022-07-03 09:36:30', '2022-07-03 09:36:30'),
+(13, 'Laptop Dell Vostroll 18', 0, 'Dell Techonology', 2000000, 14, 'Intel Core I7-1165G7', '8', 'Intel Iris XE, NVIDIA GEFORCE MX 330', '512GB', 'office', 'Laptop Laptop Dell Vostroll 18 từ nhà sản xuất Dell Techonology là một laptop hiện đại, đa dụng & rất thời trang !', '2022-07-03 09:47:39', '2022-07-03 09:47:39');
 
 -- --------------------------------------------------------
 
@@ -185,7 +207,8 @@ CREATE TABLE `ec_users` (
 
 INSERT INTO `ec_users` (`id`, `email`, `password`, `first_name`, `last_name`, `role`, `active`, `create_at`, `update_at`) VALUES
 (1, 'phongkaster@gmail.com', '$2y$10$DbMcjgmEvTO/OMcYl.HhuOKyhPkA8F4YcTQbpBJderkog.Izn0YjW', 'phong', 'kaster', 'admin', 1, '2022-06-29 20:54:20', '2022-06-29 20:54:53'),
-(4, 'phong1@gmail.com', '$2y$10$hNuPpJANZ5AuXZzdu7CwmumEcSsllzMSxuXqxubqHVFprwe7usuzm', 'Phong', 'Nguyen', 'customer', 1, '2022-07-02 07:41:35', '2022-07-02 07:41:35');
+(7, 'n18dccn147@student.ptithcm.edu.vn', '$2y$10$/mCY8QdXm3Huk1lidiorg.OmbiIwb0FfYIjdSUEUAifv7DhynATPy', 'JON', 'SNOW', 'member', 0, '2022-07-03 08:24:08', '2022-07-03 08:24:08'),
+(8, 'n18dccn148@student.ptithcm.edu.vn', '$2y$10$d4Ic6vmJ8zBvZNNLZmY.1u9iTyO1Ux52TRlwvUO8s8SEr/e/IA4XC', 'phong', 'nguyen', 'member', 0, '2022-07-02 12:27:08', '2022-07-02 12:27:08');
 
 --
 -- Indexes for dumped tables
@@ -195,6 +218,12 @@ INSERT INTO `ec_users` (`id`, `email`, `password`, `first_name`, `last_name`, `r
 -- Indexes for table `ec_categories`
 --
 ALTER TABLE `ec_categories`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `ec_configuration`
+--
+ALTER TABLE `ec_configuration`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -258,6 +287,12 @@ ALTER TABLE `ec_categories`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
+-- AUTO_INCREMENT for table `ec_configuration`
+--
+ALTER TABLE `ec_configuration`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT for table `ec_orderscontent`
 --
 ALTER TABLE `ec_orderscontent`
@@ -267,7 +302,7 @@ ALTER TABLE `ec_orderscontent`
 -- AUTO_INCREMENT for table `ec_products`
 --
 ALTER TABLE `ec_products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `ec_productsphoto`
@@ -291,7 +326,7 @@ ALTER TABLE `ec_product_category`
 -- AUTO_INCREMENT for table `ec_users`
 --
 ALTER TABLE `ec_users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- Constraints for dumped tables
